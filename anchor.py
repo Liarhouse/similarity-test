5dd0adde632f65fb5809fd4efa4618c0d03d9410
Donghwan Jung <yiijk700@gmail.com>
2023-01-29 13:28:35 +0900

    Create anchor.py

---
+++ anchor.py
@@ -0,0 +1,3 @@
+anchor = {candy:["D:\\k-digital\\source\\web_mk2\\similarity\\static\img\\similarity\\candy.jpg", 0.35],\
+    table:["D:\\k-digital\\source\\web_mk2\\similarity\\static\\img\similarity\\table.jpg",0.42],\
+        chair:["D:\k-digital\source\web_mk2\similarity\static\img\similarity\chair.jpg", 0.32]}
