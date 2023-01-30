e222e97de7129dabeeec1b1a7d92fc1289816f7f
Liarhouse <yiijk700@gmail.com>
2023-01-30 16:06:25 +0900

    Update anchor.py

--- anchor.py
+++ anchor.py
@@ -1,3 +1,15 @@
-anchor = {candy:["D:\\k-digital\\source\\web_mk2\\similarity\\static\img\\similarity\\candy.jpg", 0.35],\
-    table:["D:\\k-digital\\source\\web_mk2\\similarity\\static\\img\similarity\\table.jpg",0.42],\
-        chair:["D:\k-digital\source\web_mk2\similarity\static\img\similarity\chair.jpg", 0.32]}
+import random
+
+anchor = {'candy':["D:/k-digital/source/web_mk2/similarity/static/img/similarity/anchor/candy.jpg", '/static/img/similarity/anchor/candy.jpg', 0.35],\
+    'table':["D:/k-digital/source/web_mk2/similarity/static/img/similarity/anchor/table.jpg", "/static/img/similarity/anchor/table.jpg", 0.42],\
+        'chair':["D:/k-digital/source/web_mk2/similarity/static/img/similarity/anchor/chair.jpg", "/static/img/similarity/anchor/chair.jpg", 0.32],\
+            'stick':["D:/k-digital/source/web_mk2/similarity/static/img/similarity/anchor/stick.jpg", "/static/img/similarity/anchor/stick.jpg", 0.4],\
+                'fan':["D:/k-digital/source/web_mk2/similarity/static/img/similarity/anchor/fan.png", "/static/img/similarity/anchor/fan.png", 0.33]}
+quiz = ['candy', 'table', 'chair', 'stick', 'fan']
+
+def random_sim():
+    q = random.choice(quiz)
+    p_path = anchor[q][0]
+    h_path = anchor[q][1]
+    sim = anchor[q][2]
+    return q, p_path, h_path, sim
