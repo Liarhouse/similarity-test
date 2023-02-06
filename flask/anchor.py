223d2635c73a66dbed7a6c9df88a9e1d63252b65
Liarhouse <yiijk700@gmail.com>
2023-02-06 16:32:57 +0900

    Update anchor.py

--- flask/anchor.py
+++ flask/anchor.py
@@ -1,10 +1,10 @@
 import random
 
-anchor = {'candy':["static/img/similarity/anchor/candy.jpg", '/static/img/similarity/anchor/candy.jpg', 0.35],\
-    'table':["static/img/similarity/anchor/table.jpg", "/static/img/similarity/anchor/table.jpg", 0.42],\
-        'chair':["static/img/similarity/anchor/chair.jpg", "/static/img/similarity/anchor/chair.jpg", 0.32],\
-            'stick':["static/img/similarity/anchor/stick.jpg", "/static/img/similarity/anchor/stick.jpg", 0.4],\
-                'fan':["static/img/similarity/anchor/fan.png", "/static/img/similarity/anchor/fan.png", 0.33]}
+anchor = {'candy':["flask/static/img/similarity/anchor/candy.jpg", '/static/img/similarity/anchor/candy.jpg', 0.35],\
+    'table':["flask/static/img/similarity/anchor/table.jpg", "/static/img/similarity/anchor/table.jpg", 0.42],\
+        'chair':["flask/static/img/similarity/anchor/chair.jpg", "/static/img/similarity/anchor/chair.jpg", 0.32],\
+            'stick':["flask/static/img/similarity/anchor/stick.jpg", "/static/img/similarity/anchor/stick.jpg", 0.4],\
+                'fan':["flask/static/img/similarity/anchor/fan.png", "/static/img/similarity/anchor/fan.png", 0.33]}
 quiz = ['candy', 'table', 'chair', 'stick', 'fan']
 
 def random_sim():
